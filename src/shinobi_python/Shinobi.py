ae47a28f3437384932dc497f795a29835f316bf2
Ehsan Maiqani <spin313@gmail.com>
2022-11-06 16:19:08 +0330

    authenticate implemented

--- src/shinobi_python/Shinobi.py
+++ src/shinobi_python/Shinobi.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from dataclasses import dataclass
 
 
@@ -22,11 +24,8 @@ class Super:
             self.email = email
             self.password = password
 
-    def authenticate(self):
-        pass
-
     def url(self):
-        return f"http://{self.host}:{self.port}/super"
+        return f"http://{self.host}:{self.port}?json=true"
 
 
 @dataclass
@@ -39,22 +38,42 @@ class Admin:
     token: str = None
     email: str = None
     password: str = None
-    apikey: str = None
 
     def __init__(self, host, port, apikey=None, email=None, password=None):
         self.host = host
         self.port = port
 
         if apikey:
-            self.apikey = apikey
+            self.token = apikey
         else:
             self.email = email
             self.password = password
 
-    def authenticate(self):
-        pass
+    @property()
+    def url(self):
+        return f"http://{self.host}:{self.port}?json=true"
 
-    @property
-    def url(self) -> str:
-        return f"http://{self.host}:{self.port}"
+    def authenticate(self):
+        url = self.url()
+
+        payload = json.dumps({
+            "mail": self.email,
+            "pass": self.password
+        })
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        if response.status_code != 200:
+            raise ValueError("A problem happened in deleting user in chat service")
+
+        jsonify = response.json()
+        self.token = jsonify["auth_token"]
+
+        return jsonify["auth_token"]
+
+    
 
